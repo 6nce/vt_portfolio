2e81f4ec997b2ab0f75e9b1608f77e96b4efafd5
6nce <vincentvtruong@gmail.com>
2025-09-19 00:18:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return(
+        <main>
+            <div>
+                <h1 className="text-3xl underline">Hi, I'm Vince</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
